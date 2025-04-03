ad96b53c57f9ca52560930b068e5ebff2badcd66
sixtinhoff <franciscomsixto@gmail.com>
2025-04-03 10:34:49 -0300

    update cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: "*", // Allow all origins
+      headers: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
